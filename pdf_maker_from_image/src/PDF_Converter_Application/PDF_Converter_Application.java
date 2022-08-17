e97a729cc62ffcd21261fe631914e06d152c4ca1
Sana khan <111439733+sanakhan14@users.noreply.github.com>
2022-08-17 20:18:50 +0530

    Add files via upload

---
+++ pdf_maker_from_image/src/PDF_Converter_Application/PDF_Converter_Application.java
@@ -0,0 +1,9 @@
+package PDF_Converter_Application;
+public class PDF_Converter_Application {
+    public static void main(String[] args)
+    {
+        FrontPage p=new FrontPage();
+        p.setVisible(true);
+    }
+      
+}
